845e9cd6320905f4ce99a7349df049aae0770e7b
Zechariah Cukras <zcukrasz2@gmail.com>
2026-08-06 18:26:42 -0400

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+
+        while (l < r) {
+            int curSum = numbers[l] + numbers[r];
+
+            if (curSum > target) {
+                r--;
+            } else if (curSum < target) {
+                l++;
+            } else {
+                return new int[] { l + 1, r + 1 };
+            }
+        }
+        return new int[0];
+    }
+}
